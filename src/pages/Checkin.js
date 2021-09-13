8dbf6863ec18ff3fe716f1d972915f78b3dea018
Alejo Boga <alejoboga20@gmail.com>
2021-09-13 11:18:18 -0300

    Adding typography and useHistory for navigation

--- src/pages/Checkin.js
+++ src/pages/Checkin.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { Form, Input, Button, InputNumber } from 'antd';
+import { Form, Input, Button, InputNumber, Typography, Divider } from 'antd';
 import { SaveOutlined } from '@ant-design/icons';
+import { useHistory } from 'react-router';
+
+const { Title, Text } = Typography;
 
 const layout = {
 	labelCol: { span: 8 },
@@ -11,8 +14,11 @@ const tailLayout = {
 };
 
 export const Checkin = () => {
+	const history = useHistory();
+
 	const onFinish = (values) => {
 		console.log('Success:', values);
+		history.push('/desk');
 	};
 
 	const onFinishFailed = (errorInfo) => {
@@ -20,45 +26,50 @@ export const Checkin = () => {
 	};
 
 	return (
-		<Form
-			{...layout}
-			name='basic'
-			onFinish={onFinish}
-			onFinishFailed={onFinishFailed}
-			autoComplete='off'
-		>
-			<Form.Item
-				label='Agent Name'
-				name='agent'
-				rules={[
-					{
-						required: true,
-						message: 'Please input your username!',
-					},
-				]}
+		<>
+			<Title level={2}>Checkin</Title>
+			<Text>Type your name and desk number</Text>
+			<Divider></Divider>
+			<Form
+				{...layout}
+				name='basic'
+				onFinish={onFinish}
+				onFinishFailed={onFinishFailed}
+				autoComplete='off'
 			>
-				<Input />
-			</Form.Item>
+				<Form.Item
+					label='Agent Name'
+					name='agent'
+					rules={[
+						{
+							required: true,
+							message: 'Please input your username!',
+						},
+					]}
+				>
+					<Input />
+				</Form.Item>
 
-			<Form.Item
-				label='Desk'
-				name='desk'
-				rules={[
-					{
-						required: true,
-						message: 'Please input your desk number',
-					},
-				]}
-			>
-				<InputNumber min={1} max={99} />
-			</Form.Item>
+				<Form.Item
+					label='Desk'
+					name='desk'
+					rules={[
+						{
+							required: true,
+							message: 'Please input your desk number',
+						},
+					]}
+				>
+					<InputNumber min={1} max={99} />
+				</Form.Item>
 
-			<Form.Item {...tailLayout}>
-				<Button type='primary' htmlType='submit' shape='round'>
-					<SaveOutlined />
-					Checkin
-				</Button>
-			</Form.Item>
-		</Form>
+				<Form.Item {...tailLayout}>
+					<Button type='primary' htmlType='submit' shape='round'>
+						<SaveOutlined />
+						Checkin
+					</Button>
+				</Form.Item>
+			</Form>
+		</>
 	);
 };
